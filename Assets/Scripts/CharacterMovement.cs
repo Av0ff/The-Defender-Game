61a93c967cd762e706f438d18bd864cc481989c8
Av0ff <vingaardium@gmail.com>
2022-09-12 19:13:40 +0300

    Add: Animation

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -5,6 +5,9 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField]
     private float _speed;
 
+    [SerializeField]
+    private SpriteRenderer _characterSprite;
+
     private Rigidbody2D _rigidbodyCharacter;
 
     private Vector2 _NewPositionCharacter;
@@ -19,8 +22,15 @@ public class CharacterMovement : MonoBehaviour
     }
     private void Move(float speed)
     {
-
         _NewPositionCharacter = new Vector2(transform.position.x + Input.GetAxisRaw("Horizontal") * speed, transform.position.y + 0);
         _rigidbodyCharacter.MovePosition(_NewPositionCharacter);
+        if(Input.GetAxisRaw("Horizontal") < 0)
+        {
+            _characterSprite.flipX = true;
+        }
+        else
+        {
+            _characterSprite.flipX = false;
+        }
     }
 }
