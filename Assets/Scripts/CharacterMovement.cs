668e3dd8627c9153ffac9f4cf9b4ae819903f5db
Av0ff <vingaardium@gmail.com>
2022-09-12 03:20:11 +0300

    Add: Character Movement

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+    private Rigidbody2D _rigidbodyCharacter;
+
+    private Vector2 _NewPositionCharacter;
+
+    void Start()
+    {
+        _rigidbodyCharacter = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        Move(_speed);
+    }
+    private void Move(float speed)
+    {
+
+        _NewPositionCharacter = new Vector2(transform.position.x + Input.GetAxisRaw("Horizontal") * speed, transform.position.y + 0);
+        _rigidbodyCharacter.MovePosition(_NewPositionCharacter);
+    }
+}
